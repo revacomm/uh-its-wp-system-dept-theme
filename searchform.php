8ede11c87dc3ace0e8edb0df769b02aaed99b89b
Michelle Carino <macarino@hawaii.edu>
2018-11-09 10:20:01 -1000

    fix search by changing label for and input name attributes

--- searchform.php
+++ searchform.php
@@ -4,7 +4,7 @@
  */
 ?>
 	<form role="search" method="get" class="search-form" id="searchform" action="<?php echo esc_url( home_url( '/search/' ) ); ?>">
-		<label for="basic-site-search" class="assistive-text screen-reader-text"><?php _e( 'Search this site', 'system2018' ); ?></label>
-		<input type="search" class="search-field" name="basic-site-search" id="basic-site-search" placeholder="<?php esc_attr_e( 'Search', 'system2018' ); ?>" />
+		<label for="s" class="assistive-text screen-reader-text"><?php _e( 'Search this site', 'system2018' ); ?></label>
+		<input type="search" class="search-field" name="s" id="basic-site-search" placeholder="<?php esc_attr_e( 'Search', 'system2018' ); ?>" />
 		<button type="submit" class="search-submit" name="submit" id="searchsubmit" aria-label="search"><span class="fa fa-search" aria-hidden="true"></span><span class="screen-reader-text">Search</span></button>
 	</form>
