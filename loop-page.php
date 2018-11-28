654f87d0118ec976f48cdb03ac50ad0ee816dab6
Michelle Carino <macarino@hawaii.edu>
2018-11-27 14:01:02 -1000

    add wrapper div for featured images

--- loop-page.php
+++ loop-page.php
@@ -14,34 +14,36 @@
 
 <?php
 if ( have_posts() ) {
-	while ( have_posts() ) :
-		the_post();
-	?>
+  while ( have_posts() ) :
+    the_post();
+  ?>
 
-				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<?php if ( is_front_page() ) { ?>
-						<h2 class="entry-title"><?php the_title(); ?></h2>
-					<?php } else { ?>
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-					<?php } ?>
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <?php if ( is_front_page() ) { ?>
+            <h2 class="entry-title"><?php the_title(); ?></h2>
+          <?php } else { ?>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+          <?php } ?>
 
-					<?php the_post_thumbnail( 'full' ); ?>
+          <div class="featured-image">
+            <?php the_post_thumbnail( 'full' ); ?>
+          </div>
 
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php
-						wp_link_pages(
-							array(
-								'before' => '<div class="page-link">' . __( 'Pages:', 'system2018' ),
-								'after'  => '</div>',
-							)
-						);
-	?>
-						<?php edit_post_link( __( 'Edit', 'system2018' ), '<span class="edit-link">', '</span>' ); ?>
-					</div><!-- .entry-content -->
-				</div><!-- #post-## -->
+          <div class="entry-content">
+            <?php the_content(); ?>
+            <?php
+            wp_link_pages(
+              array(
+                'before' => '<div class="page-link">' . __( 'Pages:', 'system2018' ),
+                'after'  => '</div>',
+              )
+            );
+  ?>
+            <?php edit_post_link( __( 'Edit', 'system2018' ), '<span class="edit-link">', '</span>' ); ?>
+          </div><!-- .entry-content -->
+        </div><!-- #post-## -->
 
-				<?php comments_template( '', true ); ?>
+        <?php comments_template( '', true ); ?>
 
 <?php endwhile;
 }; // end of the loop. ?>
