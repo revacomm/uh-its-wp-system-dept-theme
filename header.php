0c911cceade05b295ebaa18aaf30059f28e2a099
Michelle Carino <macarino@hawaii.edu>
2019-01-07 15:04:49 -1000

    make separate template for duplicate search (conflicting ids)

--- header.php
+++ header.php
@@ -89,7 +89,7 @@ if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
       <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" >
         <img id="header_mid_logo" src="<?php echo get_template_directory_uri(); ?>/images/uh-nameplate.png" srcset="<?php echo get_template_directory_uri(); ?>/images/uh-nameplate.png 1x, <?php echo get_template_directory_uri(); ?>/images/uh-nameplate-2x.png 2x" alt="University of Hawai&#699;i at M&#257;noa" />
       </a>
-      <?php get_search_form(); ?>
+      <?php get_template_part('searchform', 'desktop'); ?>
     </div>
   </div>
   <div id="department_name">
