a1bc6bfd81a2c26763c7288429aad8e3887f1092
Michelle Carino <macarino@hawaii.edu>
2019-03-01 10:00:15 -1000

    remove ID from desktop searchform

--- searchform-desktop.php
+++ searchform-desktop.php
@@ -6,5 +6,5 @@
   <form method="get" class="search-form" id="searchform-desktop" aria-hidden="false" action="<?php echo esc_url( home_url( '/' ) ); ?>">
     <label for="basic-site-search-desktop" class="assistive-text screen-reader-text"><?php _e( 'Search site', 'system2018' ); ?></label>
     <input type="search" class="search-field" name="s" id="basic-site-search-desktop" placeholder="<?php esc_attr_e( 'Site search', 'system2018' ); ?>" />
-    <button type="submit" class="search-submit" name="submit" id="searchsubmit" aria-label="search" value="<?php esc_attr_e( 'Search', 'system2018' ); ?>"><span class="fa fa-search" aria-hidden="true"></span><span class="screen-reader-text">Site search</span></button>
+    <button type="submit" class="search-submit" name="submit" aria-label="search" value="<?php esc_attr_e( 'Search', 'system2018' ); ?>"><span class="fa fa-search" aria-hidden="true"></span><span class="screen-reader-text">Site search</span></button>
   </form>
