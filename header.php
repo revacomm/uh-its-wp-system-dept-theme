e71577a01ca36b3b14b253f534404c376b80eb8c
Michelle Carino <macarino@hawaii.edu>
2019-06-25 16:26:27 -1000

    move uh hard-coded header items into separate template file

--- header.php
+++ header.php
@@ -55,35 +55,7 @@ if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
 
 <body <?php body_class(); ?>>
 <header id="top">
-  <a href="#main_area" id="skip2main">Skip to Main Content</a>
-  <div id="header_top">
-    <div id="header_top_content">
-      <ul id="header_mainmenu">
-        <li><a href="https://www.hawaii.edu/">UH Home</a></li>
-        <li><a href="https://www.hawaii.edu/calendar/">Calendar</a></li>
-        <li><a href="https://www.hawaii.edu/directory/">Directory</a></li>
-        <li><a href="https://myuh.hawaii.edu/">MyUH</a></li>
-        <li><a href="http://workatuh.hawaii.edu/">Work at UH</a></li>
-      </ul>
-      <div id="header_smrow">
-        <a href="https://twitter.com/UHawaiiNews">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-twitter.png" alt="twitter" class="header_smicon" />
-        </a> &nbsp;
-        <a href="https://www.facebook.com/universityofhawaii">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-facebook.png" alt="facebook" class="header_smicon" />
-        </a> &nbsp;
-        <a href="https://instagram.com/uhawaiinews/">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-instagram.png" alt="instagram" class="header_smicon" />
-        </a> &nbsp;
-        <a href="http://www.flickr.com/photos/uhawaii">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-flickr.png" alt="flickr" class="header_smicon" />
-        </a> &nbsp;
-        <a href="https://www.youtube.com/user/uhmagazine">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-youtube.png" alt="youtube" class="header_smicon" />
-        </a>
-      </div>
-    </div>
-  </div>
+  <?php get_template_part('uh-header'); ?>
   <div id="header_mid">
     <div class="container">
       <img id="header_mid_logo" src="<?php echo get_template_directory_uri(); ?>/images/uh-nameplate.png" srcset="<?php echo get_template_directory_uri(); ?>/images/uh-nameplate.png 1x, <?php echo get_template_directory_uri(); ?>/images/uh-nameplate-2x.png 2x" alt="University of Hawai&#699;i System" />
---
+++ uh-header.php
@@ -0,0 +1,29 @@
+<a href="#main_area" id="skip2main">Skip to Main Content</a>
+  <div id="header_top">
+    <div id="header_top_content">
+      <ul id="header_mainmenu">
+        <li><a href="https://www.hawaii.edu/">UH Home</a></li>
+        <li><a href="https://www.hawaii.edu/calendar/">Calendar</a></li>
+        <li><a href="https://www.hawaii.edu/directory/">Directory</a></li>
+        <li><a href="https://myuh.hawaii.edu/">MyUH</a></li>
+        <li><a href="http://workatuh.hawaii.edu/">Work at UH</a></li>
+      </ul>
+      <div id="header_smrow">
+        <a href="https://twitter.com/UHawaiiNews">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-twitter.png" alt="twitter" class="header_smicon" />
+        </a> &nbsp;
+        <a href="https://www.facebook.com/universityofhawaii">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-facebook.png" alt="facebook" class="header_smicon" />
+        </a> &nbsp;
+        <a href="https://instagram.com/uhawaiinews/">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-instagram.png" alt="instagram" class="header_smicon" />
+        </a> &nbsp;
+        <a href="http://www.flickr.com/photos/uhawaii">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-flickr.png" alt="flickr" class="header_smicon" />
+        </a> &nbsp;
+        <a href="https://www.youtube.com/user/uhmagazine">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/icon-youtube.png" alt="youtube" class="header_smicon" />
+        </a>
+      </div>
+    </div>
+  </div>
